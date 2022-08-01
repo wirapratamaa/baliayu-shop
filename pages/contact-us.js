58ae31a9c04925c2661f93fc701edc840698f16b
wirapratamaa <wirapratama758@gmail.com>
2022-08-01 12:46:50 +0800

    add layout

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Layout from "../components/Layout";
 
 const ContactUS = () => {
-  return <div>ContactUS</div>;
+  return (
+    <Layout title={"CONTACT US PAGE"}>
+      <div>ContactUS</div>
+    </Layout>
+  );
 };
 
 export default ContactUS;
